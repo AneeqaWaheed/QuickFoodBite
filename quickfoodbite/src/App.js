3734433974c6f24e6036f872a265d55b55dfe7d3
AneeqaWaheed <aneeqawaheed19@gmail.com>
2026-05-03 23:37:10 +0500

    deployment

--- quickfoodbite/src/App.js
+++ quickfoodbite/src/App.js
@@ -5,9 +5,7 @@ import PageNotFound from "./Pages/PageNotFound";
 import Menu from "./Pages/Menu";
 import Register from "./Pages/Auth/Register";
 import Login from "./Pages/Auth/Login";
-import Cart from "./Pages/Cart";
-import Dashboard from "./Pages/User/Dashboard";
-import PrivateRoute from "./Components/Routes/Private";
+
 import ForgotPassword from "./Pages/Auth/ForgotPassword";
 import ResetPassword from "./Pages/Auth/ResetPassword";
 import AdminRoute from "./Components/Routes/AdminRoute";
@@ -15,13 +13,10 @@ import AdminDashboard from "./Pages/Admin/AdminDashboard";
 import CreateCategory from "./Pages/Admin/CreateCategory";
 import CreateProduct from "./Pages/Admin/CreateProduct";
 import Users from "./Pages/Admin/Users";
-import Orders from "./Pages/User/Orders";
-import Profile from "./Pages/User/Profile";
 import ContactPage from "./Pages/ContactPage";
 import Products from "./Pages/Admin/Products";
 import UpdateProducts from "./Pages/Admin/UpdateProduct";
 import ProductPage from "./Pages/productPage";
-import CartPage from "./Pages/Orders/orders";
 import Checkout from "./Pages/Checkout";
 import AdminOrders from "./Pages/Admin/Orders";
 import AdminCharges from "./Pages/Admin/Charges";
@@ -29,7 +24,6 @@ import ModeratorDashboard from "./Pages/Moderator/ModeratorDashboard";
 import ModeratorRoute from "./Components/Routes/ModeratorRoute";
 import ModeratorProfile from "./Pages/Moderator/ModeratorProfile";
 import ModeratorOrders from "./Pages/Moderator/ModeratorOrders";
-import ClaimOrder from "./Pages/Moderator/OrderClaimPage";
 import ClaimRedirectPage from "./Pages/Moderator/OrderClaimPage";
 
 function App() {
