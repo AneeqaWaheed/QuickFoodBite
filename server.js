88d072f4118c5920ed3115cb3fbf5df191ade726
Aneeqa Wahhed <aneeqawaheed19@gmail.com>
2026-05-03 17:51:00 +0500

    Delivery Charges

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to QUICKFOODBITE</h1>");
 });
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 //run listen
 app.listen(PORT, () => {
