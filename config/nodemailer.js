2f0961ccbede6cd01199c9173ce7518faf7804f2
AneeqaWaheed <aneeqawaheed19@gmail.com>
2026-09-23 11:48:17 +0500

    Port for Gmail

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -8,7 +8,7 @@ dotenv.config();
 const transport = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
   secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL,
